d912947421d52fad5c613aaad496de5a6bcb3205
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-04-02 10:38:19 +0200

    Dodaj pliki projektów.

---
+++ CarWorkshop/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using CarWorkshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CarWorkshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var model = new List<Person>()
+            {
+                new Person()
+                {
+                    FirstName = "Jakub",
+                    LastName = "Kozera"
+                },
+                new Person()
+                {
+                    FirstName = "Adam",
+                    LastName = "Małysz"
+                },
+            };
+
+            return View(model);
+        }
+
+        public IActionResult About()
+        {
+            var model = new About()
+            {
+                Title = "Title 1",
+                Description = "Description 1",
+                Tags = new string[] { "Tag1", "Tag 2" }
+            };
+
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CarWorkshop/Models/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace CarWorkshop.Models;
+
+public class Person
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
