139f8c43c43e39cc6ad0a0157321cd03e8084612
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-04-02 11:25:33 +0200

    Add first DB migration

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.Infrastructure.Persistence;
+
+public class CarWorkshopDbContext : DbContext
+{
+    public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-OLPFAC4;Database=CarWorkshopDb;Trusted_Connection=True;Encrypt=False");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+            .OwnsOne(c => c.ContactDetails);
+    }
+}
