33592a582c53c0695f9a1a9134486890a230c453
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-04-04 19:14:52 +0200

    Mapowanie Dto

--- CarWorkshop.Application/Extensions/ServiceCollectionExtensions.cs
+++ CarWorkshop.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using CarWorkshop.Application.Services;
+﻿using CarWorkshop.Application.Mappings;
+using CarWorkshop.Application.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CarWorkshop.Application.Extensions;
@@ -8,5 +9,7 @@ public static class ServiceCollectionExtensions
     public static void AddApplication(this IServiceCollection services)
     {
         services.AddScoped<ICarWorkshopService, CarWorkshopService>();
+
+        services.AddAutoMapper(typeof(CarWorkshopMappingProfile));
     }
 }
---
+++ CarWorkshop.Application/Mappings/CarWorkshopMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CarWorkshop.Application.CarWorkshop;
+using CarWorkshop.Domain.Entities;
+
+namespace CarWorkshop.Application.Mappings;
+
+public class CarWorkshopMappingProfile : Profile
+{
+    public CarWorkshopMappingProfile()
+    {
+        CreateMap<CarWorkshopDto, Domain.Entities.CarWorkshop>()
+            .ForMember(c => c.ContactDetails, config => config.MapFrom(dto => new CarWorkshopContactDetails()
+            {
+                City = dto.City,
+                PhoneNumber = dto.PhoneNumber,
+                PostalCode = dto.PostalCode,
+                Street = dto.Street
+            }));
+    }
+}
