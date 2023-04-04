498abb3f3402eec9c1fc2f25eaf81fc62d0f4188
lukasz-strus <61932823+lukasz-strus@users.noreply.github.com>
2023-04-04 19:53:56 +0200

    Walidacja modelu

--- CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -1,11 +1,21 @@
-﻿namespace CarWorkshop.Application.CarWorkshop;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshop.Application.CarWorkshop;
 
 public class CarWorkshopDto
 {
+    [Required(ErrorMessage = "Please insert name")]
+    [StringLength(20, MinimumLength = 2)]
     public string Name { get; set; } = default!;
+
+    [Required(ErrorMessage = "Please insert description")]
     public string? Description { get; set; }
+
     public string? About { get; set; }
+
+    [StringLength(12, MinimumLength = 8)]
     public string? PhoneNumber { get; set; }
+
     public string? Street { get; set; }
     public string? City { get; set; }
     public string? PostalCode { get; set; }
--- CarWorkshop/Controllers/CarWorkshopController.cs
+++ CarWorkshop/Controllers/CarWorkshopController.cs
@@ -21,6 +21,11 @@ public class CarWorkshopController : Controller
     [HttpPost]
     public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
     {
+        if (!ModelState.IsValid)
+        {
+            return View(carWorkshop);
+        }
+
         await _carWorkshopService.Create(carWorkshop);
 
         return RedirectToAction(nameof(Create)); //TODO refactor
